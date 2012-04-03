26cfc820c48bb74153c1e28be840ee933226f1f9
Stefan Hedman <schteppe@gmail.com>
2012-04-03 19:36:00 +0200

    doc and such

--- ghdoc.js
+++ ghdoc.js
@@ -62,6 +62,12 @@ $(function(){
 
 var GHDOC = {};
 
+/**
+ * @param string user
+ * @param string repos
+ * @param string branch
+ * @param string filename
+ */
 GHDOC.File = function(user,repos,branch,filename){
   // Get file contents
   var url = "https://github.com/api/v2/json/blob/show/"+user+"/"+repos+"/"+branch+"/"+filename;
@@ -69,12 +75,16 @@ GHDOC.File = function(user,repos,branch,filename){
       url:url,
       dataType:'jsonp',
       success:function(data){
+	console.log(GHDOC.Parse(data.blob.data));
       }
     });
 };
 
 /**
- *
+ * @param string user
+ * @param string repos
+ * @param string branch
+ * @param string name
  */
 GHDOC.Tree = function(user,repos,branch,name){
   this.filter = "*";
@@ -98,13 +108,22 @@ GHDOC.Tree = function(user,repos,branch,name){
 };
 
 /**
- * Parse source code.
+ * @fn GHDOC.Parse
+ * @brief Parse source code.
  * @param string src
+ * @return array An array of parsed objects
  */
 GHDOC.Parse = function(src){
-  src = "/**\n * @param array test A testing parameter...\n * @param int Another one\n */\nfunction test(test){};";
-  var blocks = src.match(/\/\*\*([.\n\s\t\w*@]*)\*\//gm);
+
+  var result = [];
+
+  // Get doc blocks a la doxygen
+  var blocks = src.match(/\/\*\*([.\n\s\t\r\w*@]*)\*\//gm);
+  //console.log(src);
+  console.log("Found "+(blocks ? blocks.length : 0) +" blocks");
   for(i in blocks){
+
+    // trim
     blocks[i] = blocks[i]
       .replace(/^\/\*\*[\n\t\r]*/,"")
       .replace(/[\n\t\r]*\*\/$/,"");
@@ -112,22 +131,60 @@ GHDOC.Parse = function(src){
     for(j in lines)
       lines[j] = lines[j].replace(/^[\s\t]*\*[\s\t]*/,"");
     blocks[i] = lines.join("\n");
-    var params = blocks[i].match(/@param([^@]*)/g);
-    for(j in params){
-      params[j] = params[j]
-	.replace(/[\s]*@param[\s]*/,"");
-      var s = params[j].split(" ");
-      var param = {};
-      param.type = s[0];
-      param.name = s[1];
-      s.shift(); s.shift();
-      param.desc = s.join(" ");
+
+    // Methods have "@memberof" tags to reference their class
+    var methods = blocks[i].match(/\@memberof([^@]*)/g);
+    for(j in methods){
+      methods[j] = methods[j]
+	.replace(/[\s]*@memberof[\s]*/,"");
+      var s = methods[j];
+      var m = new GHDOC.Method();
+      m.memberof = s;
+      m.parameters = GHDOC.ParseParameters(blocks[i]);
+      result.push(m);
+    }
+
+    // Classes have "@class" to define their name
+    var classes = blocks[i].match(/\@class([^@]*)/g);
+    for(j in classes){
+      classes[j] = classes[j]
+	.replace(/[\s]*@class[\s]*/,"");
+      var s = classes[j];
+      var c = new GHDOC.Class();
+      c.name = s;
+      c.parameters = GHDOC.ParseParameters(blocks[i]);
+      result.push(c);
     }
   }
+
+  return result;
 };
 
 /**
- * A representation of a class.
+ * @fn GHDOC.ParseParameters
+ * @brief Parses parameter data from a string.
+ * @param string src
+ * @return array An array of GHDOC.Parameter objects
+ */
+GHDOC.ParseParameters = function(src){
+  var result = [],
+  params = src.match(/@param([^@]*)/g);
+  for(j in params){
+    params[j] = params[j]
+      .replace(/[\s]*@param[\s]*/,"");
+    var s = params[j].split(" ");
+    var param = new GHDOC.Parameter();
+    param.type = s[0];
+    param.name = s[1];
+    s.shift(); s.shift();
+    param.desc = s.join(" ");
+  }
+  return result;
+};
+
+/**
+ * @class GHDOC.Class
+ * @brief A representation of a class.
  */
 GHDOC.Class = function(){
   this.parent = null;
@@ -137,18 +194,30 @@ GHDOC.Class = function(){
 
 /**
  * A representation of a class method.
+ * @class GHDOC.Method
  */
 GHDOC.Method = function(){
   this.name = "(untitled method)";
   this.brief = "";
   this.description = "";
   this.parameters = [];
+  this.memberof = "";
 };
 
 /**
  * A representation of a class property.
+ * @class GHDOC.Property
  */
 GHDOC.Property = function(){
+  this.type = "";
+  this.name = "";
+};
+
+/**
+ * A representation of a class property.
+ * @class GHDOC.Parameter
+ */
+GHDOC.Parameter = function(){
   this.type = "";
   this.name = "";
 };
