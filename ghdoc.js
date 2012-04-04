529d4d2cdae19619936bc4086ca4e392095ff910
Stefan Hedman <schteppe@gmail.com>
2012-04-04 09:36:01 +0200

    debug

--- ghdoc.js
+++ ghdoc.js
@@ -98,10 +98,12 @@ $(function(){
     }
 
     // Get the file tree
+    console.log("loading tree...");
     $.ajax({
 	url:"http://github.com/api/v2/json/repos/show/"+username+"/"+repository+"/branches",
 	dataType:'jsonp',
 	success:function(data){
+	  console.log(data);
 	  // Loop through branches
 	  for(branch in data.branches){
 	    console.log(branch, branchname);
